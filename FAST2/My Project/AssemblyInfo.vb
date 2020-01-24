ea396c0ba6555bc07c11cf80e19de2757cc0b278
alec-hs <6503411+alec-hs@users.noreply.github.com>
2020-01-24 01:19:02 +0000

    Version Change to 2.0.2.0
    Version change for long overdue fixes and QOL improvments.

--- FAST2/My Project/AssemblyInfo.vb
+++ FAST2/My Project/AssemblyInfo.vb
@@ -55,6 +55,6 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.1.2")>
-<Assembly: AssemblyFileVersion("2.0.1.2")>
+<Assembly: AssemblyVersion("2.0.2.0")>
+<Assembly: AssemblyFileVersion("2.0.2.0")>
 <Assembly: NeutralResourcesLanguage("en-GB")>
