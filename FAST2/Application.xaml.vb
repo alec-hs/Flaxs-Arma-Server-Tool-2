0f72563fc6db38144f7631a6f012f536f1ced0e4
Flax <alec-hs@users.noreply.github.com>
2020-07-03 21:18:43 +0100

    Add App Center Config
    Adds config to connect to MS app center for auto crash and error reporting.

--- FAST2/Application.xaml.vb
+++ FAST2/Application.xaml.vb
@@ -1,6 +1,19 @@
-﻿Class Application
+﻿Imports Microsoft.AppCenter
+Imports Microsoft.AppCenter.Analytics
+Imports Microsoft.AppCenter.Crashes
+
+
+Class Application
 
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
+
+
+    Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+        MyBase.OnStartup(e)
+        AppCenter.LogLevel = LogLevel.Verbose
+        AppCenter.Start(INSERT_APP_SECRET, GetType(Analytics), GetType(Crashes))
+
+    End Sub
 
 End Class
