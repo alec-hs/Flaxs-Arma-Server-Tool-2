64ad59a9b71b1ff2fde3844e1a9a09e4a067a1d1
Flax <alec-hs@users.noreply.github.com>
2020-07-03 22:45:49 +0100

    Version 2.0.3.1 Release
    Finalise App Center Code, change version.

--- FAST2/Application.xaml.vb
+++ FAST2/Application.xaml.vb
@@ -8,12 +8,10 @@ Class Application
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
 
-
+    'APP SECRET MUST BE REMOVED BEFORE COMMITING ONLY ADD IN PROD BUILD
     Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
         MyBase.OnStartup(e)
         AppCenter.LogLevel = LogLevel.Verbose
         AppCenter.Start(INSERT_APP_SECRET, GetType(Analytics), GetType(Crashes))
-
     End Sub
-
 End Class
