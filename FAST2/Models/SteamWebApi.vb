5e61cfecc78836fbb62563962094cdd7d2399560
Mark Leetch <markleetch@gmail.com>
2020-09-09 01:43:08 +0100

    Mistype

--- FAST2/Models/SteamWebApi.vb
+++ FAST2/Models/SteamWebApi.vb
@@ -25,7 +25,7 @@ Namespace Models
 
         'Get mod info for single mod
         Public Shared Function GetSingleFileDetails(modId As Long) As JObject
-            Dim response = ApiCall("https://api.steampowered.com/IPublishedFileService/GetDetails/v1?key=" & My.Settings.steamApiKey & V3 & modId)
+            Dim response = ApiCall("https://api.steampowered.com/IPublishedFileService/GetDetails/v1?key=" & My.Settings.SteamApiKey & V3 & modId)
 
             Return response.SelectToken("response.publishedfiledetails[0]")
         End Function
